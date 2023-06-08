cb139eddb84cb47946309fa55788117f8279a405
DipakShrestha-ADS <dipak.shrestha@eemc.edu.np>
2023-06-08 08:57:35 +0545

    portfolio complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NavBar from './navbar/NavBar';
+import Footer from './footer/Footer';
+import Body from './body/body';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <NavBar title="Dipak"/>
+    <Body/>
+    <Footer title="Dipak Shrestha"/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
